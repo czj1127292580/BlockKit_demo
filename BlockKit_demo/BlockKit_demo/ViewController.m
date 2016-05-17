ceaaecadf09f1a10255fdd0d495b3fc5ea1598ae
czj1127292580 <1127292580@qq.com>
2016-05-17 18:52:30 +0800

    update

--- BlockKit_demo/BlockKit_demo/ViewController.m
+++ BlockKit_demo/BlockKit_demo/ViewController.m
@@ -23,6 +23,23 @@
     [self.btn bk_addEventHandler:^(id sender) {
         NSLog(@"谁点我！！！");
     } forControlEvents:UIControlEventTouchUpInside];
+    
+//    UIAlertView *alertView = [UIAlertView bk_showAlertViewWithTitle:@"温馨提示"
+//                                                            message:@"你看不见我在测试吗？" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] handler:^(UIAlertView *alertView, NSInteger buttonIndex) {
+//                                                                if (buttonIndex == 0)
+//                                                                {
+//                                                                    NSLog(@"测试OK了！");
+//                                                                }
+//                                                            }];
+//    
+//    [alertView show];
+    UIAlertView *alertView = [UIAlertView bk_showAlertViewWithTitle:@"干啥" message:@"完蛋" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] handler:^(UIAlertView *alertView, NSInteger buttonIndex) {
+        
+        if (buttonIndex == 0) {
+            NSLog(@"还摸我！！！");
+        }
+    }];
+    [alertView show];
 }
 
 
