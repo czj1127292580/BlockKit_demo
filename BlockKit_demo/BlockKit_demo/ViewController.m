7dac20c41d9208fd280d56ce7d397168fa57affc
czj1127292580 <1127292580@qq.com>
2016-05-17 18:49:19 +0800

    update

--- BlockKit_demo/BlockKit_demo/ViewController.m
+++ BlockKit_demo/BlockKit_demo/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "BlocksKit+UIKit.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *btn;
 
 @end
 
@@ -17,11 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.btn bk_addEventHandler:^(id sender) {
+        NSLog(@"谁点我！！！");
+    } forControlEvents:UIControlEventTouchUpInside];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
